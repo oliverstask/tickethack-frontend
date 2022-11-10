281386f8a11cc5d8dce413bdcde32cd6eab11f63
Lumpa12 <damien.thevenin@gate-16.com>
2022-11-10 02:38:08 +0100

    ajout module à index.html

--- script.js
+++ script.js
@@ -8,17 +8,24 @@ document.querySelector("#submit").addEventListener("click", function() {
     fetch(`http://localhost:3000/trips/${departureCity}/${arrivalCity}/${departureDate}`)
     .then(response => response.json())
     .then(data => {
-        if (data.trips) {
+        if (data.result) {
             rightPanel.innerHTML = "";
-            for (let i = 0; i < data.trips.length; i++) 
-            rightPanel.innerHTML += `
-            <div class="row">
-                <p class="travel>${data.trips[i].departure} > ${data.trips[i].arrival}</p>
-                <p class="departure-hour">${data.trips[i].date}</p>
-                <p class="price">${data.trips[i].price}<p>
-                <button class="book">Réserver</button>
-            </div>
-            `;
-        }
+            for (let i = 0; i < data.trips.length; i++) {
+
+                rightPanel.innerHTML += `
+                
+                <div class="row">
+                    <p class="travel>${data.trips[i].departure} > ${data.trips[i].arrival}</p>
+                    <p class="departure-hour">${formatDepartureTime(data.trips[i].date)}</p>
+                    <p class="price">${data.trips[i].price}<p>
+                    <button class="book">Réserver</button>
+                </div>
+                `;    
+            } 
+            
+        } else {
+            document.querySelector("#train-img").src = "./images/notfound.png";
+            rightPanel.lastElementChild.textContent = "Pas de voyage trouvé."
+        }       
     });
 });
